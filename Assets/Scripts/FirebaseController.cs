bf591ba6b39eaa8b38a2bb18ef2144cc720fbd65
Ricarth Lima <rrsl@cin.ufpe.br>
2021-04-23 21:24:20 -0300

    rest load ranking working

--- Assets/Scripts/FirebaseController.cs
+++ Assets/Scripts/FirebaseController.cs
@@ -6,12 +6,13 @@ using Firebase.Database;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using UnityEngine.Networking;
+using System.Collections;
+using MiniJSON;
+using Newtonsoft.Json.Linq;
 
 public class FirebaseController : MonoBehaviour
 {
-    private FirebaseApp app;
-    private DatabaseReference mDatabaseRef;
-
     public Text txtRanking;
     public Text txtPoints;
 
@@ -21,9 +22,6 @@ public class FirebaseController : MonoBehaviour
 
     private void Start()
     {
-        FirebaseApp.DefaultInstance.Options.DatabaseUrl = new Uri("https://piranha-attack-ef368-default-rtdb.firebaseio.com/");
-        mDatabaseRef = FirebaseDatabase.DefaultInstance.RootReference;
-        InitializeBD();
     }
 
     // Update is called once per frame
@@ -37,38 +35,12 @@ public class FirebaseController : MonoBehaviour
         }
     }
 
-    private void InitializeBD()
-    {
-        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
-        {
-            var dependencyStatus = task.Result;
-            if (dependencyStatus == Firebase.DependencyStatus.Available)
-            {
-                app = FirebaseApp.DefaultInstance;
-                // Set a flag here to indicate whether Firebase is ready to use by your app.
-            }
-            else
-            {
-                UnityEngine.Debug.LogError(System.String.Format(
-                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-            }
-        });
-    }
-
-    public void RecordData(string username, int points)
-    {
-        RankingModel model = new RankingModel(username, points);
-        string json = JsonUtility.ToJson(model);
-        string uuid = GetUuid();
-        mDatabaseRef.Child("ranking").Child(uuid).SetRawJsonValueAsync(json);
-    }
-
     public string GetUuid()
     {
         string value = PlayerPrefs.GetString(PrefsKeys.uuid);
         if (value == null || value == "")
         {
-            value = System.Guid.NewGuid().ToString();
+            value = "NONE";
             PlayerPrefs.SetString(PrefsKeys.uuid, value);
         }
         return value;
@@ -76,93 +48,52 @@ public class FirebaseController : MonoBehaviour
 
     public void UpdateRankingScreen()
     {
-        FirebaseDatabase.DefaultInstance
-       .GetReference("ranking")
-       .GetValueAsync().ContinueWith(task =>
-       {
-           if (task.IsFaulted)
-           {
-               // Handle the error...
-           }
-           else if (task.IsCompleted)
-           {
-               DataSnapshot snapshot = task.Result;
-               string json = snapshot.GetRawJsonValue();
-               //print("JSON:\n" + json);
+        StartCoroutine(UpdateData());
+    }
 
-               Dictionary<string, RankingModel> dict = JsonConvert.DeserializeObject<Dictionary<string, RankingModel>>(json);
-               List<RankingModelID> list = new List<RankingModelID>();
+    private IEnumerator UpdateData()
+    {
+        String stringUsernames = "";
+        String stringPoints = "";
 
-               foreach (string key in dict.Keys)
-               {
-                   RankingModelID withId = new RankingModelID(key, dict[key].username, dict[key].points);
-                   list.Add(withId);
-               }
+        UnityWebRequest request = UnityWebRequest.Get("https://piranha-attack-api.herokuapp.com/top10/" + GetUuid());
+        request.SetRequestHeader("Authorization", Secrets.SECRET_TOKEN_API);
 
-               list.Sort((x, y) => y.points.CompareTo(x.points));
+        yield return request.SendWebRequest();
 
-               String stringUsernames = "";
-               String stringPoints = "";
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            string json = request.downloadHandler.text;
+            Debug.Log(json);
 
-               for (int i = 0; i < Math.Min(10, list.Count); i++)
-               {
-                   RankingModelID model = list[i];
-                   stringUsernames = stringUsernames + (i + 1).ToString() + ". " + model.username + "\n";
-                   stringPoints = stringPoints + model.points + "\n";
-               }
+            Dictionary<string, object> dict = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
 
-               //bool found = false;
-               //int j = 0;
-               //string myuuid = Getuuid();
-               //while (j < list.count && !found)
-               //{
-               //    if (list[j].id == myuuid)
-               //    {
-               //        found = true;
-               //    }
-               //    j++;
-               //}
+            int playerPosition = -1;
+            if (dict["player_position"] != null)
+            {
+                playerPosition = int.Parse(dict["player_position"].ToString());
+            }
 
-               //if (found)
-               //{
-               //    stringusernames = stringusernames + "\n-----------\n" + (j + 1).tostring() + ". " + list[j].username;
-               //    stringpoints = stringpoints + "\n-----------\n" + list[j].points;
-               //}
+            string microJson = dict["ranking"].ToString();
+            List<DTOScoreModel> list = JsonConvert.DeserializeObject<List<DTOScoreModel>>(microJson);
 
-               //print(stringUsernames);
-
-               this.saveRanking = stringUsernames;
-               this.savePoints = stringPoints;
-               this.isMostra = true;
-           }
-       });
+            for (int i = 0; i < list.Count; i++)
+            {
+                DTOScoreModel element = list[i];
+                stringUsernames = stringUsernames + (i + 1).ToString() + ". " + element.username + "\n";
+                stringPoints = stringPoints + element.score + "\n";
+            }
+            this.saveRanking = stringUsernames;
+            this.savePoints = stringPoints;
+            isMostra = true;
+        }
     }
 }
 
 [Serializable]
-public class RankingModel
+public class DTOScoreModel
 {
+    public Dictionary<string, object> _id;
+    public int score;
     public string username;
-    public int points;
-
-    public RankingModel(string username, int points)
-    {
-        this.username = username;
-        this.points = points;
-    }
-}
-
-[Serializable]
-public class RankingModelID
-{
-    public string id;
-    public string username;
-    public int points;
-
-    public RankingModelID(string id, string username, int points)
-    {
-        this.id = id;
-        this.username = username;
-        this.points = points;
-    }
 }
